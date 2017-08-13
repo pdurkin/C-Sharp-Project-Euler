198abd90d7bcbc072930d1faa94313558c1faf7e
Paul Durkin <PGDurkin2@aol.com>
2017-08-13 17:23:15 -0400

    started problem 10

---
+++ EulerProblem10/EulerProblem10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProblem10
+{
+    class Program
+    {
+        /*
+
+        The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+        Find the sum of all the primes below two million.
+
+        */
+        static void Main(string[] args)
+        {
+        }
+    }
+}
