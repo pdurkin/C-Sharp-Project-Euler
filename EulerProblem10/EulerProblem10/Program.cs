4f9b377f7d40155773c7ec30d31a3d2c930f2560
Paul Durkin <PGDurkin2@aol.com>
2017-08-14 11:45:27 -0400

    Euler Problem 10

--- EulerProblem10/EulerProblem10/Program.cs
+++ EulerProblem10/EulerProblem10/Program.cs
@@ -17,6 +17,38 @@ namespace EulerProblem10
         */
         static void Main(string[] args)
         {
+            long primesum = 0;
+
+            for (long i = 2; i < 2000000; i++)
+            {
+                if (isPrime(i))
+                {
+                    primesum += i;
+                }
+            }
+
+            Console.WriteLine("Sum of all primes below two million is: {0}", primesum);
+            //Answer is 142913828922
+            Console.ReadLine();
+        }
+
+        static bool isPrime(long x)
+        {
+            //we know the greatest factor of x must be no greater than its sqrt
+            long i = (long)Math.Sqrt(x);
+
+            //check to see if x is prime
+            while (i > 1)
+            {
+                if (x % i == 0)
+                {
+                    //then x is not prime
+                    return false;
+                }
+                i--;
+            }
+
+            return true;
         }
     }
 }
